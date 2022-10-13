330a5bf30e289d57f47d3cf592ec54dfb6e78bdf
MightyLex <MightyLex@gmail.com>
2022-10-13 16:08:39 +0300

    2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divGallery = document.querySelector(".gallery");
+
+const makeGalleryMarkup = function (items) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <a href="${original}" class="gallery__item"> 
+        <img src="${preview}" alt="${description}" class="gallery__image" />
+        </a>
+        `;
+    })
+    .join("");
+};
+
+divGallery.insertAdjacentHTML("beforeend", makeGalleryMarkup(galleryItems));
+console.log(divGallery);
+
+var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250  });
+
